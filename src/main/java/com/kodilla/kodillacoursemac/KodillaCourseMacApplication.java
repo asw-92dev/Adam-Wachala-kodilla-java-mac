1862ae2063598b22c6325ac4709439ae08da4ccb
asw-92dev <slip181@gmail.com>
2021-05-31 21:13:42 +0200

    First commit

---
+++ src/main/java/com/kodilla/kodillacoursemac/KodillaCourseMacApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillacoursemac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourseMacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCourseMacApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/kodillacoursemac/KodillaCourseMacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillacoursemac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaCourseMacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
