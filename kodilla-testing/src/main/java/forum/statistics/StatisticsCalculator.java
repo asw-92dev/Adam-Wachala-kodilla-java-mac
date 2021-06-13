c566b5fe1c3e719af19ecaecae53b8e70d5d2116
asw-92dev <slip181@gmail.com>
2021-06-13 11:41:19 +0200

    Task 6.7

---
+++ kodilla-testing/src/main/java/forum/statistics/StatisticsCalculator.java
@@ -0,0 +1,57 @@
+package forum.statistics;
+
+public class StatisticsCalculator {
+    private Statistics statistics;
+    private int posts;
+    private int comments;
+    private int users;
+    private double postsPerUser;
+    private double commentsPerUser;
+    private double commentsPerPost;
+
+    public StatisticsCalculator(Statistics statistics) {
+        this.statistics = statistics;
+    }
+
+    public int getPosts() {
+        return posts;
+    }
+
+    public int getComments() {
+        return comments;
+    }
+
+    public int getUsers() {
+        return users;
+    }
+
+    public double getPostsPerUser() {
+        return postsPerUser;
+    }
+
+    public double getCommentsPerUser() {
+        return commentsPerUser;
+    }
+
+    public double getCommentsPerPost() {
+        return commentsPerPost;
+    }
+
+    public void calculateAdvStatistics(Statistics statistics) {
+        posts = statistics.postsCount();
+        comments = statistics.commentsCount();
+        users = statistics.usersNames().size();
+        if (statistics.usersNames().size() == 0) {
+            postsPerUser = 0;
+            commentsPerUser = 0;
+        } else {
+            postsPerUser = statistics.postsCount() / statistics.usersNames().size();
+            commentsPerUser = statistics.commentsCount() / statistics.usersNames().size();
+        }
+        if (statistics.postsCount() == 0) {
+            commentsPerPost = 0;
+        } else {
+            commentsPerPost = statistics.commentsCount() / statistics.postsCount();
+        }
+    }
+}
