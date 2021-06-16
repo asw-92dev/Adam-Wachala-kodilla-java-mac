5cc57cc510c447f7abd651c03b10052332b185f4
asw-92dev <83578041+asw-92dev@users.noreply.github.com>
2021-06-16 08:43:30 +0200

    Module 7.1 Task: text beautifier (#8)

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -7,6 +8,7 @@ public class StreamMain {
 
     public static void main(String[] args) {
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
 
         System.out.println("Calculating expressions with lambdas");
         expressionExecutor.executeExpression(10,5, (a,b) -> a + b);
@@ -19,5 +21,12 @@ public class StreamMain {
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        System.out.println("Poem Decorator");
+        System.out.println(poemBeautifier.beautify("Kodilla.com", text -> text.toUpperCase()));
+        System.out.println(poemBeautifier.beautify("Kodilla.com", text -> text.toLowerCase()));
+        System.out.println(poemBeautifier.beautify("Kodilla.com", text -> text.repeat(3)));
+        System.out.println(poemBeautifier.beautify("Kodilla.com", text -> "ABC" + " " + text + " " + "ABC"));
+        System.out.println(poemBeautifier.beautify("Kodilla.com", text -> text.replace("", "*")));
     }
 }
