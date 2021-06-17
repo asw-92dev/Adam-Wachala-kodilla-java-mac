19fab542aa705664cbe5a199d22e96757e3162c0
asw-92dev <slip181@gmail.com>
2021-06-17 14:03:33 +0200

    module 7.3 Stream exercise

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMainNew.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import java.util.stream.Collectors;
+
+public class StreamMainNew {
+
+    public static void main(String[] args) {
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()  // [1]
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));                    // [2]
+
+        System.out.println(theResultStringOfBooks);
+    }
+}
